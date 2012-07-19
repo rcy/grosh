078d8d0fd5ebd1da860febf039b869e6c07d1b3c
Ryan Yeske <rcyeske@gmail.com>
2012-07-18 22:25:29 -0700

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grosh::Application.config.secret_token = '8369a59d67ed1fba3405dd4cf1c17317a4832cd362e70c2f6b465abd1c5b48193263fd077cc52840fcc1f25dd6c2d36633be8ca4d829ed18f2ed7b51725e4a32'
