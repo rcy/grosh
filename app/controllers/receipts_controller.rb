8e5271eebd1fd6e34428ae05fef135f10479ce28
Ryan Yeske <rcyeske@gmail.com>
2012-07-24 18:20:05 -0700

    show items with receipt

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -14,6 +14,7 @@ class ReceiptsController < ApplicationController
   # GET /receipts/1.json
   def show
     @receipt = Receipt.find(params[:id])
+    @items = @receipt.items
 
     respond_to do |format|
       format.html # show.html.erb
