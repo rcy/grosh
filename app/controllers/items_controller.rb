8b7c55e82da2080e9f7f10c6a3f3faa8d1662748
Ryan Yeske <rcyeske@gmail.com>
2012-07-26 19:59:22 -0700

    add item list

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,6 +41,7 @@ class ItemsController < ApplicationController
   # POST /items.json
   def create
     @item = Item.new(params[:item])
+    @receipt = @item.receipt
 
     respond_to do |format|
       if @item.save
